26a5c018d0a0ae617b7fd6cbfa298d156610a682
Logan Higinbotham <lgh8@pitt.edu>
2022-04-04 22:30:44 -0400

    Day 4: mining AdventCoin for Santa

---
+++ p4/2.py
@@ -0,0 +1,20 @@
+from hashlib import md5
+
+key = 'iwrupvqb'
+
+curVal = 0
+while True:
+    hash_val = md5((key + str(curVal)).encode('utf-8')).hexdigest()
+
+    found = True
+    for x in range(6):
+        if hash_val[x] != '0':
+            found = False
+            break
+
+    if found:
+        break
+
+    curVal += 1
+
+print(curVal)
